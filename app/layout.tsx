dfe0ccb6d62fe288bee9394611ca5dbaf53f8f0c
Nate <mpac4766@gmail.com>
2025-09-06 01:34:22 -0400

    New

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,11 +25,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
+    <body
+  className="min-h-screen bg-blue-900 text-gray-900 antialiased"
+>
+  {children}
+   <Analytics />
+</body>
+</html>
   );
 }
